f2ad92f8a104edd640bdf8aeb3d766376d1c82b6
Mario Celi <mcelicalderon@gmail.com>
2017-11-27 17:56:34 -0500

    Integrate input parsing and validation with calculator

--- lib/rpn-mario/calculator.rb
+++ lib/rpn-mario/calculator.rb
@@ -1,42 +1,44 @@
 module RPNMario
   class Calculator
     INVALID_ARGUMENTS_REGEX = /[^\d\s\+\-\/\*\.]/.freeze
+    ALLOWED_OPERANDS        = ['+', '-', '/', '*'].freeze
 
-    def initialize(io_interface, input_validator)
+    def initialize(io_interface)
       @io_interface    = io_interface
       @input_stack     = []
       @input_validator = Input::Validator.new(INVALID_ARGUMENTS_REGEX)
+      @input_parser    = Input::Parser.new(ALLOWED_OPERANDS)
     end
 
     def start
       while input = io_interface.read_input
-
-        # @input_stack += input.split(' ')
-        # operands = input_stack_operands
-        # next if operands.empty?
-        #
-        # result = process_input_operands
+        next unless input_valid?(input)
+        next unless parsing_valid?(input)
       end
     end
 
     private
 
-    attr_reader :input_stack, :io_interface, :input_validator
+    attr_reader :input_stack, :io_interface, :input_validator, :input_parser
 
-    # def input_stack_operands
-    #   (ALLOWED_OPERATORS & input_stack)
-    # end
-    #
-    # def process_input_operands(operands)
-    #   if operands.size == 1
-    #     single_operand_operation(input_stack.pop)
-    #   else
-    #     multiple_operand_operation(operands)
-    #   end
-    # end
-    #
-    # def single_operand_operation(operand)
-    #   input_stack.inject(0)
-    # end
+    def parsing_valid?(input)
+      parsing_result = input_parser.parse(input)
+      unless parsing_result.valid?
+        print_validation_error(parsing_result)
+      end
+      parsing_result.valid?
+    end
+
+    def input_valid?(input)
+      validation_result = input_validator.validate(input)
+      unless validation_result.valid?
+        print_validation_error(validation_result)
+      end
+      validation_result.valid?
+    end
+
+    def print_validation_error(result)
+      io_interface.display_output(result.error)
+    end
   end
 end
--- spec/rpn-mario/calculator_spec.rb
+++ spec/rpn-mario/calculator_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe RPNMario::Calculator do
   end
 
   describe '#start' do
-    context 'when input is always valid' do
-      before do
-        allow(io_interface).to receive(:read_input).and_return(
-          '1 2', '5', '+', nil
-        )
-      end
+    # context 'when input is always valid' do
+    #   before do
+    #     allow(io_interface).to receive(:read_input).and_return(
+    #       '1 2', '5.1', '+', nil
+    #     )
+    #   end
 
       # it 'outputs the result of a given operation' do
       #   expect(io_interface).to receive(:display_output).once.ordered.with(6)
@@ -23,6 +23,23 @@ RSpec.describe RPNMario::Calculator do
       #
       #   subject.start
       # end
+    # end
+    context 'when input is invalid' do
+      it 'displays an error message when input has invalid characters' do
+        allow(io_interface).to  receive(:read_input).and_return('1 2 p', '4', '1 + J', nil)
+        expect(io_interface).to receive(:display_output).once.ordered.with('Invalid characters: p')
+        expect(io_interface).to receive(:display_output).once.ordered.with('Invalid characters: J')
+
+        subject.start
+      end
+
+      it 'displays an error message when input has invalid numbers after character validation' do
+        allow(io_interface).to  receive(:read_input).and_return('1 2', '4 . .55', '1.1.5 + ', nil)
+        expect(io_interface).to receive(:display_output).once.ordered.with('Invalid operands or numbers: .')
+        expect(io_interface).to receive(:display_output).once.ordered.with('Invalid operands or numbers: 1.1.5')
+
+        subject.start
+      end
     end
   end
 end
